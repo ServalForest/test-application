3096849867d6518e248b62e2308fce9267500702
Richard Chamberlain <richarddchamberlain@gmail.com>
2023-07-10 17:48:43 +0100

    add exclamation mark

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello this is Jenny's app
+          Hello this is Jenny's app!
         </p>
         <a
           className="App-link"
