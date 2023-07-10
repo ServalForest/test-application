f449ae2f70d361118f464f4f195a2e6a45de06a8
Jennifer Travers <jennifer.travers@lendable.co.uk>
2023-07-10 17:45:08 +0100

    change name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello
+          Hello this is Jenny's app
         </p>
         <a
           className="App-link"
