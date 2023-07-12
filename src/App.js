e8ca7865ec40b205f061643a5443ae8bc588eb56
Jennifer Travers <jennifer.travers@lendable.co.uk>
2023-07-12 15:14:34 +0100

    Initial Form

--- src/App.js
+++ src/App.js
@@ -4,20 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello this is Jenny's app!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> House Price Prediction</h1>
+      <form> 
+        <div>
+        <label> Lot Area </label>
+        <input name='LotArea' type='text'></input>
+        </div>
+        <div>
+        <label> Year Built </label>
+        <input name='YearBuilt' type='text'></input>
+        </div>
+        <div>
+        <label> First Floor Square Footage </label>
+        <input name='1stFlrSF' type='text'></input>
+        </div>
+        <div>
+       <label> Second Floor Square Footage </label>
+        <input name='2ndFlrSF' type='text'></input>
+        </div>
+        <div>
+        <label> Amount of bathrooms </label>
+        <input name='FullBath' type='text'></input>
+        </div>
+        <div>
+        <label> Number of bedrooms above ground level </label>
+        <input name='BedroomAbvGr' type='text'></input>
+        </div>
+        <div>
+        <label> Total number of rooms above ground </label>
+        <input name='TotRmsAbvGrd' type='text'></input>
+        </div>
+        <button type = 'submit'>Submit 
+        </button>
+      </form>
     </div>
   );
 }
